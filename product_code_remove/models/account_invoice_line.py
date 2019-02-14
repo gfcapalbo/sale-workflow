dd9d95446340318e10b3ee0bda7518e79d394126
Giovanni Francesco Capalbo <giovanni@therp.nl>
2019-02-14 13:44:47 +0100

    [IMP] now invoice description is sale or purchase depending on invoice type

--- product_code_remove/models/account_invoice_line.py
+++ product_code_remove/models/account_invoice_line.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 # © 2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
-
     @api.multi
     @api.onchange('product_id')
     def _onchange_product_id(self):
         res = super(AccountInvoiceLine, self)._onchange_product_id()
-        if self.product_id:
-            self.name = self.product_id.name
+        for this in self:
+            part = this.invoice_id.partner_id
+            typeinv = self.invoice_id.type
+            if this.product_id:
+                if part and part.lang:
+                    product = this.product_id.with_context(lang=part.lang)
+                else:
+                    product = this.product_id
+                if typeinv in ('in_invoice', 'in_refund'):
+                    description = product.description_purchase
+                else:
+                    description = product.description_sale
+                this.name = ' '.join(
+                    [product.name or '', description or '']
+                ).strip()
         return res
